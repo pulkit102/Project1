642001bae256a456f61617fecd82b48e58bf3513
Dehelaan <pulkit22shiv@gmail.com>
2024-10-12 22:19:08 +0530

    working on URL or routes and router

--- src/app.js
+++ src/app.js
@@ -12,7 +12,15 @@ app.use(cors({
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
-
 app.use(cookieParser())
+
+//routes import
+
+import userRouter from './routes/user.routes.js';
+
+//routes declarations
+app.use("/api/v1/users",userRouter)
+
+// https://localhost:8000/api/v1/users/register
 
 export {app}
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
 /// IT IS AN HANDLER WITH THE PROMISES
 
 const asyncHandler=(requestHandler)=>{
-    (req,res,next)=>{
+    return (req,res,next)=>{
         Promise.resolve(requestHandler(req,res,next)).catch((error)=>next(error))
     }
 }
